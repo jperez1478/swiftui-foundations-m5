a3e637e2112d75cfe2326b3cb9f2b3ddfe6d9103
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-06-06 05:52:08 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Jessica Perez on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
